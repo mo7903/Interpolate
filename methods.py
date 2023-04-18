1c6967afb827c68df8cd923993c6a31fb9adc734
Mohamed Gomaa <mgomaa79@icloud.com>
2023-04-18 16:48:21 +0200

    Adjust Representation and fix Everett Interpolation function

--- methods.py
+++ methods.py
@@ -2,17 +2,17 @@ import numpy as np
 
 np.set_printoptions(suppress=True)
 
-def newton_forward(X, Y):
-    def forward_difference(x, y):
-        n = len(x)
-        # coeff is a 2D array; coeff[i, j] is the divided difference of x[i:i+j+1]
-        coeff = np.zeros((n, n))
-        coeff[:, 0] = y
-        for i in range(1, n):
-            for j in range(n - i):
-                coeff[j, i] = (coeff[j + 1, i - 1] - coeff[j, i - 1])
-        return coeff[0, :]
+def forward_difference(x, y):
+    n = len(x)
+    # coeff is a 2D array; coeff[i, j] is the divided difference of x[i:i+j+1]
+    coeff = np.zeros((n, n))
+    coeff[:, 0] = y
+    for i in range(1, n):
+        for j in range(n - i):
+            coeff[j, i] = (coeff[j + 1, i - 1] - coeff[j, i - 1])
+    return coeff
 
+def newton_forward(X, Y):
     def f(x_):
         n = len(X)
         h = np.diff(X)[0]
@@ -25,7 +25,7 @@ def newton_forward(X, Y):
         result = 0
         
         # Uses forward difference function to get the valyes    
-        forward_diff_table = forward_difference(X, Y)  
+        forward_diff_table = forward_difference(X, Y)[0, :]
 
         # Loops over number of terms
         for i in range(n):
@@ -86,60 +86,52 @@ def newton_backward(X, Y):
         return result
     return f
 
-def everett_interpolation(X, Y):
-    # Define the forward difference function
-    def forward_difference(x, y):
-        n = len(x)
-        coeff = np.zeros((n, n))
-        coeff[:, 0] = y
+import numpy as np
 
-        for j in range(1, n):
-            for i in range(n - j):
-                coeff[i][j] = coeff[i + 1][j - 1] - coeff[i][j - 1]
+def everett_interpolation(X, Y, x_):
+    """
+    A function that performs Everett interpolation for a given set of x and y values.
 
-        return coeff
+    Params:
+    X : list of x values
+    Y : list of y values corresponding to the x values
+    x_ : x value at which the interpolation is to be performed
 
-    # Define the interpolation function
-    def f(x_):
-        n = len(X)
-        h = np.diff(X)[0]
+    rtype : float
+    """
+    n = len(X)
+    h = X[1] - X[0]
 
-        # Check that all points are evenly spaced
-        assert all(np.isclose(np.diff(X), h))
+    # Create a table of differences for each order
+    diff_table = forward_difference(X, Y)
 
-        # Calculate the value of x relative to the last point
-        x_rel = x_ - X[-1]
+    # Finding the value of u
+    for i in range(0, n):
+        u = (x_ - X[i]) / h
 
-        # Calculate the value of delta_x
-        delta_x = X[1] - X[0]
+        if u == 0:
+            return Y[i]
+        elif (u > 0 and u < 1.0):
+            break
 
-        # Calculate the value of p
-        p = x_rel / delta_x
+    if i == n-1:
+        return None
 
-        # Calculate the forward difference table
-        forward_diff_table = forward_difference(X, Y)
+    z = i; w = 1 - u; i = 0
+    y_ = 0; m1 = u; m2 = w
 
-        # Initialize the result
-        result = Y[-1]
+    # Use the differences table to interpolate the y value
+    for j in range(1, n+1, 2):
+        y_ += m1 * diff_table[z+1-i, j] + m2 * diff_table[z-i, j]
+        i += 1
+        m1 *= (u - i) * (u + i) / ((j+1)*(j+2))
+        m2 *= (w - i) * (w + i) / ((j+1)*(j+2))
 
-        # Loop over the terms
-        for i in range(1, n):
-            # Initialize the numerator and denominator of the coefficient
-            num = 1
-            den = 1
+        if (z-i) < 0 or (z+1-i) > (n-j-1):
+            break
 
-            # Loop over the values of k
-            for k in range(i):
-                num *= (p - k - 1)
-                den *= (-1 - k)
+    return y_
 
-            # Calculate the coefficient
-            coeff = num / den
-
-            # Add the term to the result
-            result += coeff * forward_diff_table[-i - 1, i - 1]
-        return result
-    return f
 
 def sterling_interpolation(X, Y):
     # Define the forward difference function
--- midpoint.py
+++ midpoint.py
@@ -4,6 +4,7 @@ from methods import *
 from scipy.interpolate import pchip_interpolate
 from prettytable import PrettyTable as pt
 
+# Define the function to be interpolated
 f = lambda x: np.sin(x)
 
 # Define the range of x values
@@ -12,53 +13,59 @@ X = np.linspace(1, 10, 100)
 # Compute the true function values
 Y = [f(x) for x in X]
 
-# targets = np.linspace(2, 4, 100)
-targets = list(range(1, 10))
+# Define the range of target x values to be interpolated
+targets = np.linspace(2, 4, 50)
 
 # Compute the values using the interpolation methods
 Y_true = [f(x) for x in targets]
 Y_newton_forward = [newton_forward(X, Y)(x) for x in targets]
 Y_newton_backward = [newton_backward(X, Y)(x) for x in targets]
 Y_pchip = [pchip_interpolate(X, Y, x) for x in targets]
-Y_stirling = [sterling_interpolation(X, Y)(x) for x in targets]
-Y_everett = [everett_interpolation(X, Y)(x) for x in targets]
+Y_everett = [everett_interpolation(X, Y, x) for x in targets]
 
 # Create a table to display the results
-results = pt(["x", "True Value", "Newton's Forward", "Newton's Backward", "PCHIP", "Sterling", "Everett's"])
+results = pt(["x", "True Value", "Newton's Forward", "Newton's Backward", "PCHIP", "Everett's"])
 
 for i in range(len(targets)):
-    results.add_row([targets[i], Y_true[i], Y_newton_forward[i], Y_newton_backward[i], Y_pchip[i], Y_stirling[i], Y_everett[i]])
+    results.add_row([targets[i], Y_true[i], Y_newton_forward[i], Y_newton_backward[i], Y_pchip[i], Y_everett[i]])
 
 # Print the results table
 print(results)
 
-# Plot the true function and the interpolated functions on a common graph
-plt.plot(targets, Y_true, label='True Function')
+# Plot the true function and the interpolated functions on separate graphs
+plt.figure(figsize=(8, 6))
+plt.plot(X, Y, label='True Function')
 plt.plot(targets, Y_newton_forward, label="Newton's Forward Interpolation")
-plt.plot(targets, Y_newton_backward, label="Newton's Backward Interpolation")
+plt.legend()
+plt.title("Newton's Forward Interpolation")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+# Plot the true function and the interpolated functions on separate graphs
+plt.figure(figsize=(8, 6))
+plt.plot(X, Y, label='True Function')
+plt.plot(targets[40:], Y_newton_backward[40:], label="Newton's Backward Interpolation")
+plt.legend()
+plt.title("Newton's Backward Interpolation")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+plt.figure(figsize=(8, 6))
+plt.plot(X, Y, label='True Function')
 plt.plot(targets, Y_pchip, label='PCHIP Interpolation')
-plt.plot(targets, Y_stirling, label="Stirling Interpolation")
 plt.legend()
-#plt.show()
+plt.title('PCHIP Interpolation')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
 
-
-# Define the data points
-X = np.array([1, 2, 3, 4, 5])
-Y = np.array([f(x) for x in X])
-
-# Define the interpolation function
-f = everett_interpolation(X, Y)
-
-# Test the interpolation function
-print(f(2.5))  # Output: 6.25
-# Define the x range for plotting
-x_range = np.linspace(X[0], X[-1], num=100)
-
-# Evaluate the interpolation function over the x range
-y_range = [f(x) for x in x_range]
-
-# Plot the data points and the interpolation function
-plt.plot(X, Y, 'o', label='Data Points')
-plt.plot(x_range, y_range, label='Interpolation')
+plt.figure(figsize=(8, 6))
+plt.plot(X, Y, label='True Function')
+plt.plot(targets, Y_everett, label="Everett's Interpolation")
 plt.legend()
-#plt.show()
+plt.title("Everett's Interpolation")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
